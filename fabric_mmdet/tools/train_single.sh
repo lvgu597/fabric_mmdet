eeeaf7b064d34cbf14d92564b5ea5e691c94b9c2
lvgu <smallDou@aliyun.com>
2021-06-10 10:02:32 +0800

    mmdetection v2.12.0

---
+++ fabric_mmdet/tools/train_single.sh
@@ -0,0 +1,5 @@
+# python train.py ../config/cascade_rcnn_r50_detectors_dcn.py --gpus 1
+# python train.py ../config/cascade_rcnn_r50_detectors_70e_mixup_gc_dcnv2.py --gpus 1
+python train.py ../config/cascade_rcnn_r50_fpn_70e_733.py --gpus 1
+# python train.py ../config/cascade_rcnn_r50_detectors_dcnv2.py --gpus 1
+# python train.py ../config/cascade_rcnn_r50_detectors_70e.py --gpus 1
